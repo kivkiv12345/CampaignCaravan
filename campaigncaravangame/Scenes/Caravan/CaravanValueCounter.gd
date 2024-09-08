e2228df56bdf426a9e2bb082e7362b129b2987b8
kivkiv12345 <kevincarlsen@live.dk>
2024-09-08 14:12:33 +0200

    Fix rotation of opponent caravan value counter

--- campaigncaravangame/Scenes/Caravan/CaravanValueCounter.gd
+++ campaigncaravangame/Scenes/Caravan/CaravanValueCounter.gd
@@ -3,11 +3,15 @@ extends RichTextLabel
 class_name CaravanValueCounter
 
 @export var caravan: Caravan = null
+@export var fix_rotation: bool = true  # Allow control over whether to correct rotation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.caravan.on_value_changed.connect(self._update_shown_value)
 	self.text = String.num_int64(self.caravan.get_value())
+	if self.fix_rotation:
+		self.pivot_offset = Vector2i(self.size.x, self.size.y) / 2
+		self.rotation = -self.get_global_transform_with_canvas().get_rotation()
 
 
 func _update_shown_value(caravan: Caravan, old_value: int, new_value: int) -> void:
