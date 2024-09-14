e471c8b1a0a0add0e9205a7b47cd01e970d55d62
kivkiv12345 <57446369+kivkiv12345@users.noreply.github.com>
2024-09-14 17:56:20 +0000

    Deploying to gh-pages from @ kivkiv12345/CampaignCaravan@6050218ecb03e066a8424fa5a1aac9fa404194ae 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1726334924|13044576';
+const CACHE_VERSION = '1726336561|12419558';
 /** @type {string} */
 const CACHE_PREFIX = 'CampaignCaravan-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
