dc8eeedd36587939e10c062eb462332d6cdc9398
kivkiv12345 <kevincarlsen@live.dk>
2024-09-18 08:16:28 +0200

    Bots no longer play queens randomly
    Instead they play them last, when they can't player number cards.
    And even then they prioritize playing them on their own caravans.

--- campaigncaravangame/PlayerClasses/BotPlayer.gd
+++ campaigncaravangame/PlayerClasses/BotPlayer.gd
@@ -26,17 +26,17 @@ func perform_turn() -> void:
 	
 	for hand_card in cards:
 		
-		# TODO Kevin: Play queens as a last resort,
-		#	as they are then more likely to open up avenues of play.
-		#	We should also prefer to play them on our own caravans,
-		#	as playing them on enemy caravans might help them more than we harm them.
+		# Skip queens in this loop, so we prioritize cards that give a numeric benefit.
+		if hand_card.card.rank == Card.Rank.QUEEN:
+			continue
 		
 		var legal_slots: Array[CaravanCardSlot] = self.get_legal_slots(hand_card)
-		self._seeded_shuffle(legal_slots)
 		
 		if legal_slots.size() == 0:
 			continue  # Nowhere to play this card
-			
+
+		self._seeded_shuffle(legal_slots)
+
 		for legal_slot in legal_slots:
 			
 			# Make sure we don't ruin our own caravan
@@ -48,16 +48,16 @@ func perform_turn() -> void:
 				if hand_card.card.rank == Card.Rank.KING:
 					# Do not overburden ourselves with kings
 					if legal_slot.caravan.get_value() + (legal_slot.number_card.get_value()*2) > self.game_rules.caravan_max_value:
-						print("AAA")
+						#print("AAA")
 						continue  # Playing this king would overburden our caravan
 				# Make sure we don't accidentally 'fix' an enemy caravan
 				elif hand_card.card.rank == Card.Rank.JACK:
 					if legal_slot.caravan.get_value() <= self.game_rules.caravan_max_value:
-						print("BBB")
+						#print("BBB")
 						continue  # This caravan is not overburdened, so don't play a jack on it.
 				elif hand_card.card.is_numeric_card():
 					if (legal_slot.caravan.get_value() + hand_card.card.rank) > self.game_rules.caravan_max_value:
-						print("CCC")
+						#print("CCC")
 						continue  # Don't play number cards that would overburden our caravan
 			else:  # We are looking at an enemy caravan
 				# Make sure playing kings on the enemy actually hurts them
@@ -71,7 +71,7 @@ func perform_turn() -> void:
 					# TODO Kevin: If playing a jack on the enemy,
 					#	make sure to prioritize the most valuable cards in sold caravans.
 					if legal_slot.caravan.get_value() > self.game_rules.caravan_max_value:
-						print("EEE")
+						#print("EEE")
 						continue  # Don't play jacks on overburdended enemy caravans
 					#if (legal_slot.caravan.get_value() - legal_slot.number_card.get_value()) in range(CARAVAN_MIN_VALUE, CARAVAN_MAX_VALUE+1):
 						#continue
@@ -79,6 +79,60 @@ func perform_turn() -> void:
 			if legal_slot.try_play_card(hand_card):
 				# We played a card!
 				# We don't know where, nor what we played, but it ended our turn.
+				return
+				
+	# We couldn't play any cards, excluding queens.
+	#	So now would be a good time to play those queens.
+	#	as that might allow us to play some of our other cards.
+	for hand_card in cards:
+
+		if hand_card.card.rank != Card.Rank.QUEEN:
+			continue  # Don't play cards with didn't want to play in the for loop above.
+
+		var legal_slots: Array[CaravanCardSlot] = self.get_legal_slots(hand_card)
+		
+		if legal_slots.size() == 0:
+			continue  # Nowhere to play this queen
+
+		self._seeded_shuffle(legal_slots)
+
+		# Try playing the queen on our own caravans.
+		#	By now why can't play any other cards.
+		for legal_slot in legal_slots:
+
+			assert(legal_slot is OpenFaceCardSlot)
+
+			if legal_slot.caravan.player != self:
+				continue  # This loop is only for our own caravans
+
+			#print("IIIIII %d %d" % [legal_slot.number_card.get_index(), legal_slot.caravan.find_child("PlayedCards", false).get_child_count()])
+			assert(legal_slot.number_card in legal_slot.caravan.find_child("PlayedCards", false).get_children())
+			if legal_slot.number_card.get_index() < legal_slot.caravan.find_child("PlayedCards", false).get_child_count() - 1:
+				continue  # Only play queens at the bottom of caravans
+
+			if legal_slot.try_play_card(hand_card):
+				# We played a queen!
+				#	Hopefully we can now play a number card next round.
+				print("GGG")
+				return
+		
+		# That didn't work, let's just "discard" it, by playing it on the enemy.
+		#	We don't know what effect this will have on them however, so that's why it's our last resort.
+		for legal_slot in legal_slots:
+			
+			assert(legal_slot is OpenFaceCardSlot)
+			
+			if legal_slot.caravan.player == self:
+				continue  # This loop is only for enemy caravans
+				
+			assert(legal_slot.number_card in legal_slot.caravan.find_child("PlayedCards", false).get_children())
+			if legal_slot.number_card.get_index() < legal_slot.caravan.find_child("PlayedCards", false).get_child_count() - 1:
+				continue  # Only play queens at the bottom of caravans
+				
+			if legal_slot.try_play_card(hand_card):
+				# We played a queen, on an enemy caravan.
+				#	Hopefully that harms them, more than it helps them.
+				print("HHH")
 				return
 	
 	# We didn't have any cards to play, so we must discard something.
